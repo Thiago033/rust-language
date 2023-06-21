565f91056b57f19eb85678fecd43b165c5570b78
Thiago Lopes <thiagolopes033@hotmail.com>
2023-06-21 09:04:41 -0300

    Tutorial 6

--- tutorial006/src/main.rs
+++ tutorial006/src/main.rs
@@ -1,7 +1,14 @@
-fn main() {
-    
-    let _x: u8 = 9;  // 0 - 255
-    let _y: i8 = 10; // -128 - 127 
+use std::io;
 
+fn main() {
+    // let _x: u8 = 9;  // 0 - 255
+    // let _y: i8 = 10; // -128 - 127 
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Expected to read a line");
+
+    let int_input: i64 = input.trim().parse().unwrap();
+
+    println!("{int_input}");
     
 }
