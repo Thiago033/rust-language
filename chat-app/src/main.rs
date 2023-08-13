7b5866ec27042ed4f57373212a31ce171828543e
Thiago Lopes <88741214+Thiago033@users.noreply.github.com>
2023-08-13 20:32:46 -0300

    chat-app

---
+++ chat-app/src/main.rs
@@ -0,0 +1,45 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[cfg(test)] mod tests;
+
+use rocket::{State, Shutdown};
+use rocket::fs::{relative, FileServer};
+use rocket::form::Form;
+use rocket::response::stream::{EventStream, Event};
+use rocket::serde::{Serialize, Deserialize};
+use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
+use rocket::tokio::select;
+
+use std::sync::mpsc::channel;
+
+#[macro_use] extern crate rocket;
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[get("/")]
+fn home() -> String {
+    format!("Home page")
+}
+
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[cfg_attr(test, derive(PartialEq, UriDisplayQuery))]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    #[field(validate = len(..30))]
+    pub room: String,
+    #[field(validate = len(..20))]
+    pub username: String,
+    pub message: String,
+}
+
+
+fn main() {
+    rocket::ignite()
+        .manage(channel::<Message>(1024).0)
+        .mount("/home", routes![home]).launch();
+}
