6c6311ab61c9ca50836f539849c373e7da63a22e
Thiago Lopes <thiagolopes033@hotmail.com>
2023-08-15 15:05:49 -0300

    update

--- bevy-game/src/main.rs
+++ bevy-game/src/main.rs
@@ -1,8 +1,43 @@
-use bevy::prelude::*;
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::{CursorGrabMode, PresentMode, WindowLevel, WindowTheme},
+};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
 fn main() {
-   App::new()
-        .add_plugins(DefaultPlugins)
-        //.add_systems(Update, hello_world)
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "I am a window!".into(),
+                    resolution: (500., 300.).into(),
+                    window_theme: Some(WindowTheme::Dark),
+                    ..default()
+                }),
+                ..default()
+            }),
+        ))
+        .insert_resource(ClearColor(Color::ANTIQUE_WHITE))
+        // .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
         .run();
+}
+
+
+
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+        transform: Transform::default().with_scale(Vec3::splat(128.)),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
+        ..default()
+    });
 }
